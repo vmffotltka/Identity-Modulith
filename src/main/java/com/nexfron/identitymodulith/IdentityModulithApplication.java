38e43fd166c70a5fca1fa6bcc4c5b67515110b01
khw <khw@nexfron.com>
2025-12-11 09:59:01 +0900

    inistial commit

---
+++ src/main/java/com/nexfron/identitymodulith/IdentityModulithApplication.java
@@ -0,0 +1,13 @@
+package com.nexfron.identitymodulith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityModulithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IdentityModulithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nexfron/identitymodulith/IdentityModulithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexfron.identitymodulith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdentityModulithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
